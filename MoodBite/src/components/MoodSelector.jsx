85448f65572df42bb6733c6f2317ec6b3fceb24b
ayush shrestha <ayyush227@gmail.com>
2026-06-18 08:41:15 +0545

    moods

--- MoodBite/src/components/MoodSelector.jsx
+++ MoodBite/src/components/MoodSelector.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const MoodSelector = ({moods}) => {
+const MoodSelector = ({ moods, selectMood, customMood, setCustomMood, onMoodSelect, onCustomSubmit }) => {
   return (
-    <div>
-        
+    <div className='max-w-6xl mx-auto'>
+      <div className='grid grid-cols-3 gap-4'>
+        {moods.map((mood) => (
+          <button
+            onClick={() =>}
+            key={mood.id} className='bg-neutral-900 text-white p-10 rounded-3xl'>
+            <div className='text-3xl mb-2'>{mood.emoji}</div>
+            <div className='text-xl google-sans font-bold'>{mood.label}</div>
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
